5ec73e10b52d52f95b62ae0def36cdc29dd7cec4
Kiah Hewitt <hewittkiah@gmail.com>
2019-07-21 15:25:31 +0100

    extract item class and add test to decrease quality of item

---
+++ ruby/lib/item.rb
@@ -0,0 +1,15 @@
+require_relative 'gilded_rose'
+
+class Item
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def to_s()
+    "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
