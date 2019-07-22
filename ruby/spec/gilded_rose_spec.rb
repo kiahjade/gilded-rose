e108a79ca3197cddde42b9fe979a2d0abd901463
Kiah Hewitt <hewittkiah@gmail.com>
2019-07-22 10:53:00 +0100

    quality decreases quicker

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -21,10 +21,16 @@ describe GildedRose do
       expect(items[0].quality).to eq 0
     end
 
-    it "quality of item is never higher than 50" do
+    it "item quality is never higher than 50" do
       items = [Item.new("Aged Brie", 10, 50)]
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 50
+    end
+
+    it "decreases quality twice as much after sell by reached" do
+      items =[Item.new("toast", 0, 4)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 2
     end
   end
 
